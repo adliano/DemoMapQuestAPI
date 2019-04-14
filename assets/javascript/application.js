cf4e446ef1e176693464735ffcc79f0eb108f69e
Adliano Alves <adliano@me.com>
2019-04-14 01:25:29 -0700

    add comments

--- assets/javascript/application.js
+++ assets/javascript/application.js
@@ -11,27 +11,33 @@ let apiParams = {
 function getGeoLocation() {
     // if geolocation is available in browser
     if (navigator.geolocation) {
+        // call getCity with geo possition
         navigator.geolocation.getCurrentPosition(getCity);
     }
 }
 
 function getCity(userPosition) {
-
+    // get latitude
     let lat = userPosition.coords.latitude;
+    // get longitude
     let lng = userPosition.coords.longitude;
+    // add location to apiParams
     apiParams.location = `${lat},${lng}`
-    console.log(`%c location : ${apiParams.location}`, `background-color: blue; color:white;`);
-
+    // create the url parameters
     let searchParams = new URLSearchParams(apiParams);
-
-
+    // send request to API  
     fetch(`${url}${searchParams}`)
+        // get the api response and parse to JSON
         .then((apiResponse) => apiResponse.json())
+        // get the results (by API docs its a array)
         .then((jsonObj) => jsonObj.results[0])
+        // get the locations (by API docs its a array)
         .then((result) => result.locations[0])
+        // set city and state
         .then((location) => {
             let currentCity = `${location.adminArea5}, ${location.adminArea3}`;
             document.querySelector('#header').innerHTML = currentCity;
+            document.querySelector('#map').setAttribute(`src`, location.mapUrl);
         });
 }
 
